901f9605adc7c6c769c268e838cec29ff1c0f70f
Mark Brough <mark.brough@gmail.com>
2016-01-18 13:07:24 +0100

    Get CRS codelists

--- get.sh
+++ get.sh
@@ -1,1 +1,2 @@
 wget "https://www.iana.org/assignments/media-types/media-types.xml" -O source/media-types.xml
+wget "http://www.oecd.org/dac/stats/documentupload/Codelist.xls" -O source/CRS-codelists.xls
